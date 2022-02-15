2ce26637f81c0ef9211a807118e40081a1766da0
kira-0521 <kiratanaka0521@gmail.com>
2022-02-15 22:31:18 +0900

    feature: todo更新処理追加

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/item', [ItemController::class, 'index']);
+Route::get('/items', [ItemController::class, 'index']);
 Route::prefix('/item')->group(function() {
     Route::post('/store', [ItemController::class, 'store']);
     Route::put('/{id}', [ItemController::class, 'update']);
