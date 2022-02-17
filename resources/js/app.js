a559e5c0876a32de6b7931ffc3a6fc85bc8a6dfd
kira-0521 <kiratanaka0521@gmail.com>
2022-02-17 22:52:00 +0900

    add: vueと連携して画面表示するまで

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import { createApp } from "vue";
+// import { router } from "vue-router";
+import App from "./vue/app";
+
+const app = createApp(App);
+// app.use(router);
+app.mount("#app");
---
+++ resources/js/vue/app.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>vue3 starting!!!</div>
+</template>
